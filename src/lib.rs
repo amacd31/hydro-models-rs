1273e58ccd62dca8b5475b20495a93340f42bd23
Andrew MacDonald <andrew@maccas.net>
2022-04-09 13:17:18 +1000

    Store paramters in their own struct

--- src/lib.rs
+++ src/lib.rs
@@ -27,11 +27,16 @@ pub mod hydromodels {
     }
 
     #[repr(C)]
-    pub struct GR4JModel {
+    pub struct GR4JParams {
         pub x1: f64,
         pub x2: f64,
         pub x3: f64,
         pub x4: f64,
+    }
+
+    #[repr(C)]
+    pub struct GR4JModel {
+        pub params: GR4JParams,
         pub production_store: f64,
         pub routing_store: f64,
         pub uh1: Vec<f64>,
@@ -45,10 +50,12 @@ pub mod hydromodels {
                 // Perrin, Charles, Claude Michel, and Vazken Andréassian.
                 // "Improvement of a parsimonious model for streamflow simulation."
                 // Journal of Hydrology 279, no. 1 (2003): 275-289.
-                x1: 350.,
-                x2: 0.,
-                x3: 90.,
-                x4: 1.7,
+                params: GR4JParams {
+                    x1: 350.,
+                    x2: 0.,
+                    x3: 90.,
+                    x4: 1.7,
+                },
 
                 // Completely dry initial catchment
                 production_store: 0.,
@@ -73,20 +80,20 @@ pub mod hydromodels {
         pub fn run(&mut self, precip: &[f64], potential_evap: &[f64]) -> Vec<f64> {
             let mut qsim: Vec<f64> = Vec::with_capacity(precip.len());
 
-            let n_uh1 = self.x4.ceil() as i32;
-            let n_uh2 = (2.0 * self.x4).ceil() as i32;
+            let n_uh1 = self.params.x4.ceil() as i32;
+            let n_uh2 = (2.0 * self.params.x4).ceil() as i32;
 
             let mut uh1_ordinates = vec![0.; n_uh1 as usize];
             let mut uh2_ordinates = vec![0.; n_uh2 as usize];
 
             for t in 1..(n_uh1 + 1) {
-                uh1_ordinates[(t - 1) as usize] =
-                    s_curves1(f64::from(t), self.x4) - s_curves1(f64::from(t) - 1., self.x4);
+                uh1_ordinates[(t - 1) as usize] = s_curves1(f64::from(t), self.params.x4)
+                    - s_curves1(f64::from(t) - 1., self.params.x4);
             }
 
             for t in 1..(n_uh2 + 1) {
-                uh2_ordinates[(t - 1) as usize] =
-                    s_curves2(f64::from(t), self.x4) - s_curves2(f64::from(t) - 1., self.x4);
+                uh2_ordinates[(t - 1) as usize] = s_curves2(f64::from(t), self.params.x4)
+                    - s_curves2(f64::from(t) - 1., self.params.x4);
             }
 
             for (p, e) in precip.iter().zip(potential_evap) {
@@ -95,21 +102,23 @@ pub mod hydromodels {
                 let reservoir_production;
                 if p > e {
                     net_evap = 0.;
-                    let scaled_net_precip = (13.0f64).min((p - e) / self.x1);
+                    let scaled_net_precip = (13.0f64).min((p - e) / self.params.x1);
                     let tanh_scaled_net_precip = scaled_net_precip.tanh();
-                    reservoir_production = (self.x1
-                        * (1. - (self.production_store / self.x1).powf(2.))
+                    reservoir_production = (self.params.x1
+                        * (1. - (self.production_store / self.params.x1).powf(2.))
                         * tanh_scaled_net_precip)
-                        / (1. + self.production_store / self.x1 * tanh_scaled_net_precip);
+                        / (1. + self.production_store / self.params.x1 * tanh_scaled_net_precip);
 
                     routing_pattern = p - e - reservoir_production;
                 } else {
-                    let scaled_net_evap = (13.0f64).min((e - p) / self.x1);
+                    let scaled_net_evap = (13.0f64).min((e - p) / self.params.x1);
                     let tanh_scaled_net_evap = scaled_net_evap.tanh();
 
-                    let ps_div_x1 = (2. - self.production_store / self.x1) * tanh_scaled_net_evap;
+                    let ps_div_x1 =
+                        (2. - self.production_store / self.params.x1) * tanh_scaled_net_evap;
                     net_evap = self.production_store * (ps_div_x1)
-                        / (1. + (1. - self.production_store / self.x1) * tanh_scaled_net_evap);
+                        / (1.
+                            + (1. - self.production_store / self.params.x1) * tanh_scaled_net_evap);
 
                     reservoir_production = 0.;
                     routing_pattern = 0.;
@@ -118,7 +127,7 @@ pub mod hydromodels {
                 self.production_store = self.production_store - net_evap + reservoir_production;
 
                 let percolation = self.production_store
-                    / (1. + (self.production_store / 2.25 / self.x1).powf(4.)).powf(0.25);
+                    / (1. + (self.production_store / 2.25 / self.params.x1).powf(4.)).powf(0.25);
                 routing_pattern += self.production_store - percolation;
 
                 self.production_store = percolation;
@@ -141,12 +150,13 @@ pub mod hydromodels {
                     *last_uh2 = *last_ordinate * routing_pattern;
                 }
 
-                let groundwater_exchange = self.x2 * (self.routing_store / self.x3).powf(3.5);
+                let groundwater_exchange =
+                    self.params.x2 * (self.routing_store / self.params.x3).powf(3.5);
                 self.routing_store =
                     (0.0f64).max(self.routing_store + self.uh1[0] * 0.9 + groundwater_exchange);
 
-                let r2 =
-                    self.routing_store / (1. + (self.routing_store / self.x3).powf(4.)).powf(0.25);
+                let r2 = self.routing_store
+                    / (1. + (self.routing_store / self.params.x3).powf(4.)).powf(0.25);
                 let qr = self.routing_store - r2;
                 self.routing_store = r2;
                 let qd = (0.0f64).max(self.uh2[0] * 0.1 + groundwater_exchange);
@@ -164,10 +174,10 @@ pub mod hydromodels {
             routing_store: Option<f64>,
             unit_hydrographs: Option<HashMap<&str, Vec<f64>>>,
         ) {
-            self.x1 = params["X1"];
-            self.x2 = params["X2"];
-            self.x3 = params["X3"];
-            self.x4 = params["X4"];
+            self.params.x1 = params["X1"];
+            self.params.x2 = params["X2"];
+            self.params.x3 = params["X3"];
+            self.params.x4 = params["X4"];
 
             if let Some(ps) = production_store {
                 self.production_store = ps;
@@ -177,8 +187,8 @@ pub mod hydromodels {
                 self.routing_store = rs;
             }
 
-            let n_uh1 = self.x4.ceil() as usize;
-            let n_uh2 = (2.0 * self.x4).ceil() as usize;
+            let n_uh1 = self.params.x4.ceil() as usize;
+            let n_uh2 = (2.0 * self.params.x4).ceil() as usize;
 
             // Default to all zeroes but set after if defined in unit_hydrographs HashMap
             self.uh1 = vec![0.; n_uh1];
